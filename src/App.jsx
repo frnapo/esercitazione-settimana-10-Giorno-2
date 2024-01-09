af2989a48ca0f5822408b3a8ae7134fdf0f415c1
Francesco <napo.fra96@gmail.com>
2024-01-09 17:16:25 +0100

    update app & commentarea

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { Component } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import MyNav from "./components/MyNav";
 import MyFooter from "./components/MyFooter";
@@ -7,35 +6,32 @@ import BookList from "./components/BookList";
 import CommentArea from "./components/CommentArea";
 import fantasy from "./data/fantasy.json";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
 
-class App extends Component {
-  state = {
-    selectedAsin: null,
+const App = () => {
+  const [selectedAsin, setSelectedAsin] = useState(null);
+
+  const selectBook = (asin) => {
+    setSelectedAsin(asin);
   };
 
-  selectBook = (asin) => {
-    this.setState({ selectedAsin: asin });
-  };
-
-  render() {
-    return (
-      <>
-        <MyNav />
-        <Container>
-          <Welcome />
-          <Row>
-            <Col xs={8}>
-              <BookList books={fantasy} onSelectBook={this.selectBook} selectedAsin={this.state.selectedAsin} />
-            </Col>
-            <Col xs={4}>
-              <CommentArea asin={this.state.selectedAsin} />
-            </Col>
-          </Row>
-        </Container>
-        <MyFooter />
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <MyNav />
+      <Container>
+        <Welcome />
+        <Row>
+          <Col xs={8}>
+            <BookList books={fantasy} onSelectBook={selectBook} selectedAsin={selectedAsin} />
+          </Col>
+          <Col xs={4}>
+            <CommentArea asin={selectedAsin} />
+          </Col>
+        </Row>
+      </Container>
+      <MyFooter />
+    </>
+  );
+};
 
 export default App;
--- src/components/CommentArea.jsx
+++ src/components/CommentArea.jsx
@@ -1,47 +1,52 @@
+import { useEffect, useState } from "react";
 import CommentList from "./CommentList";
 import AddComment from "./AddComment";
 import Loading from "./Loading";
 import Error from "./Error";
-import { useEffect, useState } from "react";
 
 const CommentArea = ({ asin }) => {
-  const [comments, setComments] = useState({
-    comments: [],
-    isLoading: true,
-    isError: false,
-  });
+  const [comments, setComments] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
   useEffect(() => {
-    fetchComments();
-  }, [asin]);
-
-  const fetchComments = async () => {
-    try {
-      let response = await fetch("https://striveschool-api.herokuapp.com/api/comments/" + asin, {
-        headers: {
-          Authorization:
-            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTliZmRlYmUwZGQxZDAwMTgyZDE3NjgiLCJpYXQiOjE3MDQ3MjE4OTksImV4cCI6MTcwNTkzMTQ5OX0.KUa3ZuV_ghbFwVz_BnfoQ5cTvW0KWN-D73DAZ1a0Ebw",
-        },
-      });
-      console.log(response);
-      if (response.ok) {
-        let comments = await response.json();
-        setComments({ comments: comments, isLoading: false, isError: false });
-      } else {
-        setComments({ isLoading: false, isError: true });
+    const getComments = async () => {
+      setIsLoading(true);
+      try {
+        let response = await fetch("https://striveschool-api.herokuapp.com/api/comments/" + asin, {
+          headers: {
+            Authorization:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTlkNmY1Y2U2Mjg4NjAwMTg4M2Y2ZWQiLCJpYXQiOjE3MDQ4MTY0NzYsImV4cCI6MTcwNjAyNjA3Nn0._AAZZ0SkMGkVBf2y3rV6dp08ZPJ90nFXZkhPm-g02yY",
+          },
+        });
+        console.log(response);
+        if (response.ok) {
+          let comments = await response.json();
+          setComments(comments);
+          setIsLoading(false);
+          setIsError(false);
+        } else {
+          console.log("error");
+          setIsLoading(false);
+          setIsError(true);
+        }
+      } catch (error) {
+        console.log(error);
+        setIsLoading(false);
+        setIsError(true);
       }
-    } catch (error) {
-      console.log(error);
-      setComments({ isLoading: false, isError: true });
+    };
+    if (asin) {
+      getComments();
     }
-  };
+  }, [asin]);
 
   return (
     <div className="text-center mt-5 pt-3">
-      {comments.isLoading && <Loading />}
-      {comments.isError && <Error />}
-      {asin && <AddComment asin={asin} />}
-      {asin && <CommentList commentsToShow={comments.comments} />}
+      {isLoading && <Loading />}
+      {isError && <Error />}
+      <AddComment asin={asin} />
+      <CommentList commentsToShow={comments} />
     </div>
   );
 };
