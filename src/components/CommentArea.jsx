d9f4aa031e9ab228c7b43a1dc6cc9846ac89665a
Francesco <napo.fra96@gmail.com>
2024-01-09 16:19:54 +0100

    Update CommentArea.jsx

--- src/components/CommentArea.jsx
+++ src/components/CommentArea.jsx
@@ -1,25 +1,23 @@
-import { Component } from "react";
 import CommentList from "./CommentList";
 import AddComment from "./AddComment";
 import Loading from "./Loading";
 import Error from "./Error";
+import { useEffect, useState } from "react";
 
-class CommentArea extends Component {
-  state = {
+const CommentArea = ({ asin }) => {
+  const [comments, setComments] = useState({
     comments: [],
     isLoading: true,
     isError: false,
-  };
+  });
 
-  componentDidUpdate(prevProps) {
-    if (prevProps.asin !== this.props.asin) {
-      this.fetchComments();
-    }
-  }
+  useEffect(() => {
+    fetchComments();
+  }, [asin]);
 
-  fetchComments = async () => {
+  const fetchComments = async () => {
     try {
-      let response = await fetch("https://striveschool-api.herokuapp.com/api/comments/" + this.props.asin, {
+      let response = await fetch("https://striveschool-api.herokuapp.com/api/comments/" + asin, {
         headers: {
           Authorization:
             "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTliZmRlYmUwZGQxZDAwMTgyZDE3NjgiLCJpYXQiOjE3MDQ3MjE4OTksImV4cCI6MTcwNTkzMTQ5OX0.KUa3ZuV_ghbFwVz_BnfoQ5cTvW0KWN-D73DAZ1a0Ebw",
@@ -28,26 +26,24 @@ class CommentArea extends Component {
       console.log(response);
       if (response.ok) {
         let comments = await response.json();
-        this.setState({ comments: comments, isLoading: false, isError: false });
+        setComments({ comments: comments, isLoading: false, isError: false });
       } else {
-        this.setState({ isLoading: false, isError: true });
+        setComments({ isLoading: false, isError: true });
       }
     } catch (error) {
       console.log(error);
-      this.setState({ isLoading: false, isError: true });
+      setComments({ isLoading: false, isError: true });
     }
   };
 
-  render() {
-    return (
-      <div className="text-center">
-        {this.state.isLoading && <Loading />}
-        {this.state.isError && <Error />}
-        {this.props.asin && <AddComment asin={this.props.asin} />}
-        {this.props.asin && <CommentList commentsToShow={this.state.comments} />}
-      </div>
-    );
-  }
-}
+  return (
+    <div className="text-center mt-5 pt-3">
+      {comments.isLoading && <Loading />}
+      {comments.isError && <Error />}
+      {asin && <AddComment asin={asin} />}
+      {asin && <CommentList commentsToShow={comments.comments} />}
+    </div>
+  );
+};
 
 export default CommentArea;
